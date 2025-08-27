e3051b18bb5c609825fefac23ef9e1c2247de410
The iOS Team @ GeneXus <builder@genexus.com>
2025-08-27 10:55:15 -0400

    Package version: 3.2.0-beta.38

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXUCLinearGaugeWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "3.2.0-beta.37")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "3.2.0-beta.38")
 	],
 	targets: [
 		.target(name: "GXUCLinearGaugeWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCLinearGauge",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCLinearGauge-3.2.0-beta.37.xcframework.zip",
-			checksum: "673ca94c69e2042a9405b570a90966c13310bf9128266213b8c5e7cdb0abab59"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCLinearGauge-3.2.0-beta.38.xcframework.zip",
+			checksum: "247c5b8709362c06cb1d8249761fe3deac8bd748a8c90af1157317e678c4305d"
 		)
 	]
 )
